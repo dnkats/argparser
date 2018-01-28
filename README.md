87c14db8b9e3b2bf2915668279003f001afe5720
Daniel Kats <dnkats@gmail.com>
2018-01-28 18:09:36 +0100

    Add the parser and an example

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # argparser
 
 Argument list parser class
+
+  * loop through "-" and "--" options using `nextoption(option)`
+  * get arguments after the current option using `optarg(arg)`
+  * mark this argument as a part of the options using `markasoption()`
+  * loop through remaining arguments using `nextremaining(arg)`
+  
+See main.cpp for an example.
+
+    ./a.out -pd test1 -vx -1 test bla
+    
+Output:
+
+    Option: p
+    Option: d
+    Verbosity: -1
+    Option: x
+     Remaining arguments: test1
+     Remaining arguments: test
+     Remaining arguments: bla
---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <iostream>
+#include "argpars.h"
+
+int main(int argc, char **argv)
+{
+  ArgPars args(argc,argv);
+  std::string opt, arg;
+  // handle options  
+  while ( args.nextoption(opt) ) {
+    if ( opt == "h" || opt == "-help" ) {
+      std::cout << "Help file" << std::endl;
+      return 0;
+    } else if ( opt == "v" || opt == "-verbose" ) {
+      if ( args.optarg(arg) ){
+        std::cout << "Verbosity: " << arg << std::endl;
+        args.markasoption();
+      }
+    } else {
+        std::cout << "Option: " << opt << std::endl;
+    }
+  }
+  while( args.nextremaining(arg) ) {
+    std::cout << " Remaining arguments: " << arg << std::endl;
+  } 
+  return 0;
+}
